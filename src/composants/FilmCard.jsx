6ec912c344fba12d00b15363537ae2cfc5773ef9
jlnko <jlnko@hotmail.fr>
2024-05-07 14:13:58 +0200

    composant filmCard créer, affichage de la liste des films réussi

--- src/composants/FilmCard.jsx
+++ src/composants/FilmCard.jsx
@@ -1,9 +1,18 @@
+import React from "react"
 
-
-function FilmCard() {
+const FilmCard = ({film}) => {
   return (
-    <div>
-      
+    <div className="film">
+        <p className="film-date">{film.Year}</p>
+        <img
+        src={film.Poster !== 'N/A' ? film.Poster : 'https://via.placeholder.com/300'}
+        alt={film.Title}
+        className="film-img" />
+      <div className="filter"></div>
+      <div className="film-info">
+      <span>{film.Type}</span>
+      <h3>{film.Title}</h3>
+      </div>
     </div>
   )
 }
